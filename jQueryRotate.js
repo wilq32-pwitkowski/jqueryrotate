4a2f1c51b9a5f5ba63027b2de44eccd313202b14
wilq32 <wilq32@05f192a8-ba21-6a86-2ae6-0c881fb7de60>
2010-08-21 09:48:24 +0000

    - refactored and reviewed code (teft some TODO's), removed lot of unused code, - increased performance for firefox, - hopefully fixed IE problem with positioning (1px difference that is visible especially for very small images), - fixed problem when setting an angle and it animates to angle 0 (instead of staying in place) - compressed size down from 7kb to 5kb,

--- jQueryRotate.js
+++ jQueryRotate.js
@@ -1,4 +1,4 @@
-// VERSION: 1.5 LAST UPDATE: 26.07.2010
+// VERSION: 1.6 LAST UPDATE: 21.08.2010
 /*
  * THIS IS FREE SCRIPT BUT LEAVE THIS COMMENT IF
  * YOU WANT USE THIS CODE ON YOUR SITE
@@ -86,18 +86,20 @@ Examples:
 (function($) {
 var supportedCSS,styles=document.getElementsByTagName("head")[0].style,toCheck="transformProperty WebkitTransform OTransform".split(" "); //MozTransform <- firefox works slower with css!!!
 for (var a=0;a<toCheck.length;a++) if (styles[toCheck[a]] !== undefined) supportedCSS = toCheck[a];
+var IE = "v"=="\v";
 
 jQuery.fn.extend({
 ImageRotate:function(parameters)
-{	
+{
+	// If this element is already a Wilq32.PhotoEffect object, skip creation
 	if (this.Wilq32&&this.Wilq32.PhotoEffect) return;
-	var paramClone = $.extend(true, {}, parameters)
+	// parameters might be applied to many objects - so because we use them later - a fresh instance is needed 
+	var paramClone = $.extend(true, {}, parameters); 
 	return (new Wilq32.PhotoEffect(this.get(0),paramClone))._temp;
 },
 rotate:function(parameters)
 {
-	if (this.length===0) return;
-	if (typeof parameters=="undefined") return;
+	if (this.length===0||typeof parameters=="undefined") return;
 	if (typeof parameters=="number") parameters={angle:parameters};
 	var returned=[];
 	for (var i=0,i0=this.length;i<i0;i++)
@@ -115,8 +117,7 @@ rotate:function(parameters)
 
 rotateAnimation:function(parameters)
 {
-	if (this.length===0) return;
-	if (typeof parameters=="undefined") return;
+	if (this.length===0||typeof parameters=="undefined") return;
 	if (typeof parameters=="number") parameters={animateAngle:parameters};
 	var returned=[];
 	for (var i=0,i0=this.length;i<i0;i++)
@@ -134,240 +135,245 @@ rotateAnimation:function(parameters)
 
 });
 
-Wilq32={};
+// Library agnostic interface
 
+Wilq32=window.Wilq32||{};
 Wilq32.PhotoEffect=(function(){
-	if (supportedCSS) 
-	return function(img,parameters){
+	function setupParameters(img,parameters){
 		this._img = img;
 		this._parameters = parameters || {};
-		this._parameters.animateAngle = parameters.animateAngle || 0;
 		this._parameters.angle = this._angle = parameters.angle || 0;
-		img.Wilq32 = {
-			PhotoEffect: this
-		};
-		this._BindEvents(this._img,this._parameters.bind);
-		this._rotate(this._parameters.angle);
-		if (this._parameters.angle!=this._parameters.animateAngle) this.rotateAnimation(this._parameters);
+		this._parameters.animateAngle = typeof parameters.animateAngle=="number" ? parameters.animateAngle : this._angle;
+		
 	}
-	
-	else 
-	return function(img,parameters)
-	{
-				this._img=img;
-				this._parameters=parameters || {};
-				this._parameters.className=img.className;
-				this._parameters.id=img.getAttribute('id');
-				this._parameters.animateAngle = parameters.animateAngle ||0;				
-				this._parameters.angle = this._angle = parameters.angle || 0;
-				this._temp=document.createElement('span');
-				this._temp.style.display="inline-block";
-				this._temp.Wilq32 = 
-					{
-						PhotoEffect: this
-					};			
-				img.parentNode.insertBefore(this._temp,img);
-				this._img._ref=this;
-				if (this._img.complete) {
-					this._Loader();
-				}
-				else{
-					jQuery(this._img).bind("load", function()
-					{
-						this._ref._Loader.call(this._ref);
-					});
-				}
+	if (supportedCSS) {
+		return function(img,parameters){
+			setupParameters.call(this,img,parameters);
+			img.Wilq32 = {
+				PhotoEffect: this
+			};
+			// TODO: needed to have a _temp variable accessible outside - used for object retrieval, 
+			//        needs refactor + change name (temp is not self descriptive)
+			// also need better passing values between functions - to FIX (remove _temp and _img at all)
+			this._temp = this._img;
+			this._BindEvents(img,this._parameters.bind);
+			this._rotate(this._parameters.angle);
+			if (this._parameters.angle!=this._parameters.animateAngle) this.rotateAnimation(this._parameters);
+		}
+	} else {
+		return function(img,parameters) {
+			setupParameters.call(this,img,parameters);			
+			// Make sure that class and id are also copied - just in case you would like to refeer to an newly created object
+			this._parameters.className=img.className;
+			this._parameters.id=img.getAttribute('id');
+
+			this._temp=document.createElement('span');
+			this._temp.style.display="inline-block";
+			this._temp.Wilq32 = 
+				{
+					PhotoEffect: this
+				};
+			img.parentNode.insertBefore(this._temp,img);
+			
+			if (img.complete) {
+				this._Loader();
+			} else {
+				var self=this;
+				// TODO: Remove jQuery dependency
+				jQuery(this._img).bind("load", function()
+				{
+					self._Loader();
+				});
+			}
+		}
 	}
 })();
 
-Wilq32.PhotoEffect.prototype.rotateAnimation = function(parameters){
-	this._parameters.animateAngle = parameters.animateAngle;
-	this._parameters.callback = parameters.callback || this._parameters.callback || function(){};
-	this._animateStart();
+Wilq32.PhotoEffect.prototype={
+
+	rotateAnimation : function(parameters){
+		this._parameters.animateAngle = parameters.animateAngle;
+		this._parameters.callback = parameters.callback || this._parameters.callback || function(){};
+		this._animateStart();
+	},
+
+	_BindEvents:function(element,events){
+		if (events) 
+		{
+			for (var a in events) if (events.hasOwnProperty(a)) 
+				for (var b in events[a]) if (events[a].hasOwnProperty(b)) 
+				// TODO: Remove jQuery dependency
+					jQuery(element).bind(b,events[a][b]);
+		}
+	},
+
+	_Loader:(function()
+	{
+		if (IE)
+		return function()
+		{
+			var width=this._img.width;
+			var height=this._img.height;
+			this._img.parentNode.removeChild(this._img);
+							
+			this._vimage = this.createVMLNode('image');
+			this._vimage.src=this._img.src;
+			this._vimage.style.height=height+"px";
+			this._vimage.style.width=width+"px";
+			this._vimage.style.position="absolute"; // FIXES IE PROBLEM - its only rendered if its on absolute position!
+			this._vimage.style.top = "0px";
+			this._vimage.style.left = "0px";
+
+			/* Group minifying a small 1px precision problem when rotating object */
+			this._container =  this.createVMLNode('group');
+			this._container.style.width=width;
+			this._container.style.height=height;
+			this._container.style.position="absolute";
+			this._container.setAttribute('coordsize',width-1+','+(height-1)); // This -1, -1 trying to fix that ugly problem
+			this._container.appendChild(this._vimage);
+			
+			this._temp.appendChild(this._container);
+			this._temp.style.position="relative"; // FIXES IE PROBLEM
+			this._temp.style.width=width+"px";
+			this._temp.style.height=height+"px";
+			this._temp.setAttribute('id',this._parameters.id);
+			this._temp.className=this._parameters.className;			
+			
+			this._BindEvents(this._temp,this._parameters.bind);
+			_finally.call(this);
+			
+		}
+		else
+		return function ()
+		{
+			this._temp.setAttribute('id',this._parameters.id);
+			this._temp.className=this._parameters.className;
+			
+			this._width=this._img.width;
+			this._height=this._img.height;
+			this._widthHalf=this._width/2; // used for optimisation
+			this._heightHalf=this._height/2;// used for optimisation
+			
+			var _widthMax=Math.sqrt((this._height)*(this._height) + (this._width) * (this._width));
+
+			this._widthAdd = _widthMax - this._width;
+			this._heightAdd = _widthMax - this._height;	// widthMax because maxWidth=maxHeight
+			this._widthAddHalf=this._widthAdd/2; // used for optimisation
+			this._heightAddHalf=this._heightAdd/2;// used for optimisation
+			
+			this._img.parentNode.removeChild(this._img);	
+
+
+			this._canvas=document.createElement('canvas');
+			this._canvas.setAttribute('width',this._width);
+			this._canvas.style.position="relative";
+			this._canvas.style.left = -this._widthAddHalf + "px";
+			this._canvas.style.top = -this._heightAddHalf + "px";
+			this._canvas.Wilq32 = this._temp.Wilq32;
+			
+			this._temp.appendChild(this._canvas);
+			this._temp.style.width=this._width+"px";
+			this._temp.style.height=this._height+"px";
+			
+			this._BindEvents(this._canvas,this._parameters.bind);
+			this._cnv=this._canvas.getContext('2d');
+			_finally.call(this);
+		}
+		function _finally(){
+			this._rotate(this._parameters.angle);
+			if (this._parameters.angle!=this._parameters.animateAngle) this.rotateAnimation(this._parameters);		
+		}
+
+	})(),
+
+	_animateStart:function()
+	{	
+		if (this._timer) {
+			clearTimeout(this._timer);
+		}
+		this._animate();
+	},
+	_animate:function()
+	{
+		var checkEnd = !!(Math.round(this._angle * 100 - this._parameters.animateAngle * 100)) == 0 && !!this._timer;
+
+		if (this._parameters.callback && checkEnd){
+			this._parameters.callback();
+		}
+
+		// TODO: Bug for animatedGif for static rotation ? (to test)
+		if (checkEnd && !this._parameters.animatedGif) 
+			{
+				clearTimeout(this._timer);
+			}
+			else 
+			{
+				if (this._canvas||this._vimage||this._img) {
+					// TODO: implement easing and speed of animation
+					this._angle-=(this._angle-this._parameters.animateAngle)*0.1;
+					if (typeof this._parameters.minAngle!="undefined") this._angle=Math.max(this._angle,this._parameters.minAngle);
+					if (typeof this._parameters.maxAngle!="undefined") this._angle=Math.min(this._angle,this._parameters.maxAngle);
+					this._rotate((~~(this._angle*10))/10);
+				}
+				var self = this;
+				this._timer = setTimeout(function()
+				{
+					self._animate.call(self);
+				}, 10);
+			}
+	},
+
+	_rotate : (function()
+	{
+		var rad = Math.PI/180;
+		if (IE)
+		return function(angle)
+		{
+			this._container.style.rotation=angle+"deg";
+		}
+		else if (supportedCSS)
+		return function(angle){
+			this._img.style[supportedCSS]="rotate("+angle+"deg)";
+		}
+		else 
+		return function(angle)
+		{
+
+			if (!this._img.width||typeof angle!="number") return;
+			angle=(angle%360)* rad;
+			// clear canvas	
+			this._canvas.width = this._width+this._widthAdd;
+			this._canvas.height = this._height+this._heightAdd;
+			
+			//TODO: Implement scaling for fixed image size
+			//this._cnv.scale(0.8,0.8); // SCALE - if needed ;)
+			
+			// REMEMBER: all drawings are read from backwards.. so first function is translate, then rotate, then translate, translate..
+			this._cnv.translate(this._widthAddHalf,this._heightAddHalf);	// at least center image on screen
+			this._cnv.translate(this._widthHalf,this._heightHalf);			// we move image back to its orginal 
+			this._cnv.rotate(angle);										// rotate image
+			this._cnv.translate(-this._widthHalf,-this._heightHalf);		// move image to its center, so we can rotate around its center
+			this._cnv.drawImage(this._img, 0, 0);							// First - we draw image
+		}
+
+	})()
 }
 
-if (jQuery.browser.msie)
+if (IE)
 {
 Wilq32.PhotoEffect.prototype.createVMLNode=(function(){
 document.createStyleSheet().addRule(".rvml", "behavior:url(#default#VML)");
-        try {
-            !document.namespaces.rvml && document.namespaces.add("rvml", "urn:schemas-microsoft-com:vml");
-            return function (tagName) {
-                return document.createElement('<rvml:' + tagName + ' class="rvml">');
-            };
-        } catch (e) {
-            return function (tagName) {
-                return document.createElement('<' + tagName + ' xmlns="urn:schemas-microsoft.com:vml" class="rvml">');
-            };
-        }
-		
+		try {
+			!document.namespaces.rvml && document.namespaces.add("rvml", "urn:schemas-microsoft-com:vml");
+			return function (tagName) {
+				return document.createElement('<rvml:' + tagName + ' class="rvml">');
+			};
+		} catch (e) {
+			return function (tagName) {
+				return document.createElement('<' + tagName + ' xmlns="urn:schemas-microsoft.com:vml" class="rvml">');
+			};
+		}		
 })();
 }
 
-Wilq32.PhotoEffect.prototype._BindEvents=function(element,events){
-	if (events) 
-	{
-		for (var a in events) if (events.hasOwnProperty(a)) 
-			for (var b in events[a]) if (events[a].hasOwnProperty(b)) 
-				jQuery(element).bind(b,events[a][b]);
-	}
-}
-
-Wilq32.PhotoEffect.prototype._Loader=
-(function()
-{
-	if (jQuery.browser.msie)
-	return function()
-	{
-		var src=this._img.src;
-		//this._IEfix.src=""; // Fixes endless loading
-		this._temp.setAttribute('id',this._parameters.id);
-		this._temp.className=this._parameters.className;
-		var width=this._img.width;
-		var height=this._img.height;
-		this._img.parentNode.removeChild(this._img);
-			
-		this._img._widthMax=this._img._heightMax=Math.sqrt((height)*(height) + (width) * (width));
-		this._img._heightMax=Math.sqrt((height)*(height) + (width) * (width));
-		
-		this._vimage = this.createVMLNode('image');
-		this._vimage._ref=this;
-		this._vimage.style.height=height+"px";
-		this._vimage.style.width=width+"px";
-		this._temp.style.position="relative"; // FIXES IE PROBLEM
-		this._vimage.style.position="absolute"; // FIXES IE PROBLEM - its only rendered if its on absolute position!
-		
-		this._temp.style.width=this._temp.style.height=this._img._heightMax+"px";
-		this._vimage.src=src;
-		this._temp.appendChild(this._vimage);
-		//this._vimage.outerHTML = this._vimage.outerHTML;
-		//if (this._parameters.preservePosition){
-			this._temp.style.width=width+"px";
-			this._temp.style.height=height+"px";
-			this._vimage.style.top = "0px";
-			this._vimage.style.left = "0px";
-		/*} else {
-			this._vimage.style.top = (this._img._heightMax-height)/2;
-			this._vimage.style.left = (this._img._widthMax-width)/2;
-		}*/
-		
-		var self = this;
-		this._BindEvents(this._temp,this._parameters.bind);
-		this._rotate(this._parameters.angle);
-		if (this._parameters.angle!=this._parameters.animateAngle) this.rotateAnimation(this._parameters);
-		
-	}
-	else
-	return function ()
-	{
-		this._temp.setAttribute('id',this._parameters.id);
-		this._temp.className=this._parameters.className;
-		var width=this._img.width;
-		var height=this._img.height;
-		this._img.parentNode.removeChild(this._img);	
-		this._img._widthMax=this._img._heightMax=Math.sqrt((height)*(height) + (width) * (width));
-
-		this._canvas=document.createElement('canvas');
-		this._canvas._ref=this;
-		this._canvas.height=height;
-		this._canvas.width=width;
-
-		this._canvas.setAttribute('width',width);
-
-		this._canvas.Wilq32 = this._temp.Wilq32;	
-		this._temp.appendChild(this._canvas);
-		//if (this._parameters.preservePosition){
-			this._temp.style.width=width+"px";
-			this._temp.style.height=height+"px";
-			this._canvas.style.position="relative";
-			this._canvas.style.left = -(this._img._widthMax - width)/2 + "px";
-			this._canvas.style.top = -(this._img._widthMax - height)/2 + "px";
-		//}
-
-		var self = this;
-		this._BindEvents(this._canvas,this._parameters.bind);
-		this._cnv=this._canvas.getContext('2d');
-		this._rotate(this._parameters.angle);
-		if (this._parameters.angle!=this._parameters.animateAngle) this.rotateAnimation(this._parameters);
-	}
-
-})();
-
-Wilq32.PhotoEffect.prototype._animateStart=function()
-{	
-	if (this._timer) {
-		clearTimeout(this._timer);
-	}
-	this._animate();
-}
-Wilq32.PhotoEffect.prototype._animate=function()
-{
-	if (this._canvas||this._vimage||this._img) this._angle-=(this._angle-this._parameters.animateAngle)*0.1;
-	if (typeof this._parameters.minAngle!="undefined") if (this._angle<this._parameters.minAngle) this._angle=this._parameters.minAngle;
-	if (typeof this._parameters.maxAngle!="undefined") if (this._angle>this._parameters.maxAngle) this._angle=this._parameters.maxAngle; 
-	var checkEnd = !!(Math.round(this._angle * 100 - this._parameters.animateAngle * 100)) == 0 && !!this._timer;
-
-	if (this._parameters.callback && checkEnd){
-		this._parameters.callback();
-	}
-	
-	if (checkEnd && !this._parameters.animatedGif) 
-		{
-			clearTimeout(this._timer);
-		}
-		else 
-		{
-			if (this._canvas||this._vimage||this._img) this._rotate((~~(this._angle*10))/10);
-			var self = this;
-			this._timer = setTimeout(function()
-			{
-				self._animate.call(self);
-			}, 10);
-		}
-}
-
-Wilq32.PhotoEffect.prototype._rotate = (function()
-{
-	if (jQuery.browser.msie)
-	return function(angle)
-	{
-		this._vimage.style.rotation=angle;
-		//var radians=angle*Math.PI/180;
-		//this._vimage.style.top=  (this._img._heightMax - this._img.height)/2- (this._vimage.offsetHeight-this._img.height)/2 +"px";
-		//this._vimage.style.left= (this._img._widthMax - this._img.width)/2- (this._vimage.offsetWidth-this._img.width)/2 +"px";
-	}
-	else if (supportedCSS)
-	return function(angle){
-		this._img.style[supportedCSS]="rotate("+angle+"deg)";
-	}
-	else 
-	return function(angle)
-
-	{
-
-		if (!this._img.width) return;
-		if (typeof angle!="number") return;
-		angle=(angle%360)* Math.PI / 180;
-		var width=this._img.width;
-		var height=this._img.height;
-		var widthAdd = this._img._widthMax - width;
-		var heightAdd = this._img._heightMax - height;
-		// clear canvas	
-		this._canvas.width = width+widthAdd;
-		this._canvas.height = height+heightAdd;
-
-		//this._cnv.scale(0.8,0.8); // SCALE - if needed ;)
-		
-		// REMEMBER: all drawings are read from backwards.. so first function is translate, then rotate, then translate, translate..
-		this._cnv.save();
-		this._cnv.translate(widthAdd/2,heightAdd/2); // at least center image on screen
-		this._cnv.translate(width/2,height/2);		  // we move image back to its orginal 
-		this._cnv.rotate(angle);					  // rotate image
-		this._cnv.translate(-width/2,-height/2);	  // move image to its center, so we can rotate around its center
-		this._cnv.drawImage(this._img, 0, 0);		  // First - we draw image
-		this._cnv.restore();
-	}
-
-
-})();
 })(jQuery);
