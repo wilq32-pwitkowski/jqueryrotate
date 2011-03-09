5ac7edc9ddadb809c4a27316c31c551f4c4e4503
wilq32 <wilq32@05f192a8-ba21-6a86-2ae6-0c881fb7de60>
2011-03-09 21:29:31 +0000

    API changes:
    - no more rotateAnimation function! Now instead of typing rotateAnimation(10) you simply write rotate({animateTo:10}), this clears alot with different usages as 'angle' parameter always directly changes rotation and 'animateTo' always triggers animate rotation from current angle (or given angle),
    
    before:
    
    
      $("#image").rotateAnimation(360);
    
    or
    
      $("#image").rotateAnimation({animateAngle:360});
    
    now:
    
    $("#image").rotate({animateTo:360});
    
    NOTE! There is now no option to animate using only a integer (you have to use an object with animateTo option at least)
    
    - binding function now does not require an array, just an object with hash (previous concept was... just stupid):
    
    before:
    
      $("#image").rotate({ angle: 10, bind: [ 
          {
            "click":function(){}
          },
          {
            "mouseover":function(){}
          } 
      ] })
    
    now:
    
      $("#image").rotate({ angle: 10, bind:
          {
            "click":function(){},
            "mouseover":function(){}
          } 
        })
    
    Other changes:
    
    - Rebinding events, now you can bind new events so old one get disconnected without anything,
    - Cleaned lot of code (smaller is better),
    - Fixed problem with recursive bind calls,
    - Thanks to Mark Hunter now last step of animation rotates to a requested value (instead of 'almost there'),
    - Removed documentation from source code as it was outdated, hard to use and maitain (use wiki project page),
    - Fixed few problems with parameter parsing/handling by changing API concept

--- trunk/jQueryRotate.js
+++ trunk/jQueryRotate.js
@@ -1,91 +1,12 @@
-// VERSION: 1.7 LAST UPDATE: 16.12.2010
+// VERSION: 1.8 LAST UPDATE: 9.03.2011
 /* 
  * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
  * 
  * Made by Wilq32, wilq32@gmail.com, Wroclaw, Poland, 01.2009
  * Website: http://code.google.com/p/jqueryrotate/ 
  */
-/*
-Description:
 
-This is an final product of a Wilq32.PhotoEffect Snippet. Actually you can
- use this simple and tiny script to get effect of rotated images directly 
- from client side (for ex. user generated content), and animate them using
- own functions. 
-
-
-Notices:
-
-Include script after including main jQuery. Whole plugin uses jQuery
-namespace and should be compatible with older version (unchecked). 
-
-Usage:
-
-jQuery(imgElement).rotate(angleValue)
-jQuery(imgElement).rotate(parameters)
-jQuery(imgElement).rotateAnimation(parameters)
-jQuery(imgElement).rotateAnimation(parameters)
-
-
-
-Returns:
-
-jQueryRotateElement - !!! NOTICE !!! function return rotateElement
-instance to help connect events with actually created 'rotation' element.
-
-Parameters:
-
-    ({angle:angleValue,
-     [animateAngle:animateAngleValue],
-	 [easing:easingFunction],
-	 [duration:durationValue],
-     !!! DEPRECATED !!!    [maxAngle:maxAngleValue],
-     !!! DEPRECATED !!!    [minAngle:minAngleValue],
-     [callback:callbackFunction],
-	 [animatedGif:animatedGifBoolean],
-     [bind:[{event: function},{event:function} ] })
-jQuery(imgElement).rotateAnimation
-
-Where:
-
-- angleValue - clockwise rotation given in degrees,
-- [animateAngleValue] - optional parameter, animate rotating into this value,
-- [easing] - optional parameter, function to control animation speed - supports native easing
-             function and a jquery easing plugin, default: easeOutQuart
-- [duration] - optional parameter, duration of a animation - default 1000ms 
-- [maxAngleValue] - !!! DEPRECATED !!!    optional parameter, maximum angle possible for animation,
-- [minAngleValue] - !!! DEPRECATED !!!    optional parameter, minimum angle possible for animation,
-- [callbackFunction] - optional function to run after animation is done
-- [animatedGifBoolean](boolean)  - optional set to display animated gif in firefox/chrome/safari
-            !!! this might slow down browser because it need to render image again and
-			again to display animation,
-- [bind: [ {event: function}...] -optional parameter, list of events binded
-  to newly created rotateable object
-
-Examples:
-
-		$(document).ready(function()
-		{
-			$('#image').rotate(-25);			
-		});
-
-		$(document).ready(function()
-		{
-			$('#image2').rotate({angle:5});	
-		});
-
-		$(document).ready(function()
-		{
-			var rot=$('#image3').rotate({maxAngle:25,minAngle:-55, duration:570,
-			easing:$.easing.easeInOutExpo,
-			bind:
-				[
-					{"mouseover":function(){rot[0].rotateAnimation(85);}},
-					{"mouseout":function(){rot[0].rotateAnimation(-35);}}
-				]
-			});
-		});
-*/
+// Documentation removed from script file (was kinda useless and outdated)
 
 (function($) {
 var supportedCSS,styles=document.getElementsByTagName("head")[0].style,toCheck="transformProperty WebkitTransform OTransform msTransform".split(" "); //MozTransform <- firefox works slower with css!!!
@@ -101,7 +22,7 @@ ImageRotate:function(parameters)
 	if (this.Wilq32&&this.Wilq32.PhotoEffect) return;
 	// parameters might be applied to many objects - so because we use them later - a fresh instance is needed 
 	var paramClone = $.extend(true, {}, parameters); 
-	return (new Wilq32.PhotoEffect(this.get(0),paramClone))._temp;
+	return (new Wilq32.PhotoEffect(this.get(0),paramClone))._rootObj;
 },
 rotate:function(parameters)
 {
@@ -114,82 +35,47 @@ rotate:function(parameters)
 		if (typeof element.Wilq32 == "undefined") 
 			returned.push($($(element).ImageRotate(parameters)));
 		else 
-		{
-			element.Wilq32.PhotoEffect._rotate(parameters.angle);
-		}
-	}
-	return returned;
-},
-
-rotateAnimation:function(parameters)
-{
-	if (this.length===0||typeof parameters=="undefined") return;
-	if (typeof parameters=="number") parameters={animateAngle:parameters};
-	var returned=[];
-	for (var i=0,i0=this.length;i<i0;i++)
-	{	
-	    var element=this.get(i);
-		if (typeof element.Wilq32 == "undefined") 
-			returned.push($($(element).ImageRotate(parameters)));
-		else 
-		{
-			element.Wilq32.PhotoEffect.rotateAnimation(parameters);
-		}
+            element.Wilq32.PhotoEffect._handleRotation(parameters);
 	}
 	return returned;
 }
-
 });
 
 // Library agnostic interface
 
 Wilq32=window.Wilq32||{};
 Wilq32.PhotoEffect=(function(){
-	function setupParameters(img,parameters){
-		this._img = img;
-		this._parameters = parameters || {};
-		this._parameters.angle = this._angle = parameters.angle || 0;
-		this._parameters.animateAngle = typeof parameters.animateAngle=="number" ? parameters.animateAngle : this._angle;
-		this._parameters.easing = parameters.easing || function (x, t, b, c, d) { return -c * ((t=t/d-1)*t*t*t - 1) + b; }
-		this._parameters.duration = parameters.duration || 1000;
-	}
+
 	if (supportedCSS) {
 		return function(img,parameters){
-			setupParameters.call(this,img,parameters);
 			img.Wilq32 = {
 				PhotoEffect: this
 			};
-			// TODO: needed to have a _temp variable accessible outside - used for object retrieval, 
-			//        needs refactor + change name (temp is not self descriptive)
-			// also need better passing values between functions - to FIX (remove _temp and _img at all)
-			this._temp = this._img;
-			this._BindEvents(img,this._parameters.bind);
-			this._rotate(this._parameters.angle);
-			if (this._parameters.angle!=this._parameters.animateAngle) this.rotateAnimation(this._parameters);
+            
+            this._img = this._rootObj = this._eventObj = img;
+            this._handleRotation(parameters);
 		}
 	} else {
 		return function(img,parameters) {
-			setupParameters.call(this,img,parameters);			
 			// Make sure that class and id are also copied - just in case you would like to refeer to an newly created object
-			this._parameters.className=img.className;
-			this._parameters.id=img.getAttribute('id');
+            this._img = img;
 
-			this._temp=document.createElement('span');
-			this._temp.style.display="inline-block";
-			this._temp.Wilq32 = 
+			this._rootObj=document.createElement('span');
+			this._rootObj.style.display="inline-block";
+			this._rootObj.Wilq32 = 
 				{
 					PhotoEffect: this
 				};
-			img.parentNode.insertBefore(this._temp,img);
+			img.parentNode.insertBefore(this._rootObj,img);
 			
 			if (img.complete) {
-				this._Loader();
+				this._Loader(parameters);
 			} else {
 				var self=this;
 				// TODO: Remove jQuery dependency
 				jQuery(this._img).bind("load", function()
 				{
-					self._Loader();
+					self._Loader(parameters);
 				});
 			}
 		}
@@ -197,27 +83,48 @@ Wilq32.PhotoEffect=(function(){
 })();
 
 Wilq32.PhotoEffect.prototype={
+    _setupParameters : function (parameters){
+		this._parameters = this._parameters || {_angle:0, animateTo:0};
+        if (typeof parameters.angle==="number") this._angle = parameters.angle;
+        this._parameters.animateTo = (typeof parameters.animateTo==="number") ? parameters.animateTo : this._angle; 
 
-	rotateAnimation : function(parameters){
-		this._parameters.animateAngle = parameters.animateAngle;
-		this._parameters.callback = parameters.callback || this._parameters.callback || function(){};	
-		this._animateStart();
+		this._parameters.easing = parameters.easing || this._parameters.easing || function (x, t, b, c, d) { return -c * ((t=t/d-1)*t*t*t - 1) + b; }
+		this._parameters.duration = parameters.duration || this._parameters.duration || 1000;
+        this._parameters.callback = parameters.callback || this._parameters.callback || function(){};
+        if (parameters.bind && parameters.bind != this._parameters.bind) this._BindEvents(parameters.bind); 
+	},
+	_handleRotation : function(parameters){
+          this._setupParameters(parameters);
+          if (this._angle==this._parameters.animateTo) {
+              this._rotate(this._angle);
+          }
+          else { 
+              this._animateStart();          
+          }
 	},
 
-	_BindEvents:function(element,events){
-		if (events) 
+	_BindEvents:function(events){
+		if (events && this._eventObj) 
 		{
+            // Unbinding previous Events
+            if (this._parameters.bind){
+                var oldEvents = this._parameters.bind;
+                for (var a in oldEvents) if (oldEvents.hasOwnProperty(a)) 
+                        // TODO: Remove jQuery dependency
+                        jQuery(this._eventObj).unbind(a,oldEvents[a]);
+            }
+
+            this._parameters.bind = events;
 			for (var a in events) if (events.hasOwnProperty(a)) 
-				for (var b in events[a]) if (events[a].hasOwnProperty(b)) 
 				// TODO: Remove jQuery dependency
-					jQuery(element).bind(b,events[a][b]);
+					jQuery(this._eventObj).bind(a,events[a]);
 		}
 	},
 
 	_Loader:(function()
 	{
 		if (IE)
-		return function()
+		return function(parameters)
 		{
 			var width=this._img.width;
 			var height=this._img.height;
@@ -236,25 +143,23 @@ Wilq32.PhotoEffect.prototype={
 			this._container.style.width=width;
 			this._container.style.height=height;
 			this._container.style.position="absolute";
-			this._container.setAttribute('coordsize',width-1+','+(height-1)); // This -1, -1 trying to fix that ugly problem
+			this._container.setAttribute('coordsize',width-1+','+(height-1)); // This -1, -1 trying to fix ugly problem with small displacement on IE
 			this._container.appendChild(this._vimage);
 			
-			this._temp.appendChild(this._container);
-			this._temp.style.position="relative"; // FIXES IE PROBLEM
-			this._temp.style.width=width+"px";
-			this._temp.style.height=height+"px";
-			this._temp.setAttribute('id',this._parameters.id);
-			this._temp.className=this._parameters.className;			
-			
-			this._BindEvents(this._temp,this._parameters.bind);
-			_finally.call(this);
-			
+			this._rootObj.appendChild(this._container);
+			this._rootObj.style.position="relative"; // FIXES IE PROBLEM
+			this._rootObj.style.width=width+"px";
+			this._rootObj.style.height=height+"px";
+			this._rootObj.setAttribute('id',this._img.getAttribute('id'));
+			this._rootObj.className=this._img.className;			
+		    this._eventObj = this._rootObj;	
+		    this._handleRotation(parameters);	
 		}
 		else
-		return function ()
+		return function (parameters)
 		{
-			this._temp.setAttribute('id',this._parameters.id);
-			this._temp.className=this._parameters.className;
+			this._rootObj.setAttribute('id',this._img.getAttribute('id'));
+			this._rootObj.className=this._img.className;
 			
 			this._width=this._img.width;
 			this._height=this._img.height;
@@ -278,21 +183,16 @@ Wilq32.PhotoEffect.prototype={
 			this._canvas.style.position="relative";
 			this._canvas.style.left = -this._widthAddHalf + "px";
 			this._canvas.style.top = -this._heightAddHalf + "px";
-			this._canvas.Wilq32 = this._temp.Wilq32;
+			this._canvas.Wilq32 = this._rootObj.Wilq32;
 			
-			this._temp.appendChild(this._canvas);
-			this._temp.style.width=this._width+"px";
-			this._temp.style.height=this._height+"px";
+			this._rootObj.appendChild(this._canvas);
+			this._rootObj.style.width=this._width+"px";
+			this._rootObj.style.height=this._height+"px";
+            this._eventObj = this._canvas;
 			
-			this._BindEvents(this._canvas,this._parameters.bind);
 			this._cnv=this._canvas.getContext('2d');
-			_finally.call(this);
+            this._handleRotation(parameters);
 		}
-		function _finally(){
-			this._rotate(this._parameters.angle);
-			if (this._parameters.angle!=this._parameters.animateAngle) this.rotateAnimation(this._parameters);		
-		}
-
 	})(),
 
 	_animateStart:function()
@@ -304,37 +204,36 @@ Wilq32.PhotoEffect.prototype={
 		this._animateStartAngle = this._angle;
 		this._animate();
 	},
-	_animate:function()
-	{
-		var actualTime = +new Date;
-		//var checkEnd = !!(Math.round(this._angle * 100 - this._parameters.animateAngle * 100)) == 0 && !!this._timer;
-		var checkEnd = actualTime - this._animateStartTime > this._parameters.duration;
-		if (this._parameters.callback && checkEnd){
-			this._parameters.callback();
-		}
+_animate:function()
+     {
+         var actualTime = +new Date;
+         var checkEnd = actualTime - this._animateStartTime > this._parameters.duration;
 
-		// TODO: Bug for animatedGif for static rotation ? (to test)
-		if (checkEnd && !this._parameters.animatedGif) 
-			{
-				clearTimeout(this._timer);
-			}
-			else 
-			{
-				if (this._canvas||this._vimage||this._img) {
-					// TODO: implement easing and speed of animation
-					this._angle = this._parameters.easing(0, actualTime - this._animateStartTime, this._animateStartAngle, this._parameters.animateAngle - this._animateStartAngle, this._parameters.duration);
-					//this._angle-=(this._angle-this._parameters.animateAngle)*0.1;
-					//if (typeof this._parameters.minAngle!="undefined") this._angle=Math.max(this._angle,this._parameters.minAngle);
-					//if (typeof this._parameters.maxAngle!="undefined") this._angle=Math.min(this._angle,this._parameters.maxAngle);
-					this._rotate((~~(this._angle*10))/10);
-				}
-				var self = this;
-				this._timer = setTimeout(function()
-				{
-					self._animate.call(self);
-				}, 10);
-			}
-	},
+         // TODO: Bug for animatedGif for static rotation ? (to test)
+         if (checkEnd && !this._parameters.animatedGif) 
+         {
+             clearTimeout(this._timer);
+         }
+         else 
+         {
+             if (this._canvas||this._vimage||this._img) {
+                 var angle = this._parameters.easing(0, actualTime - this._animateStartTime, this._animateStartAngle, this._parameters.animateTo - this._animateStartAngle, this._parameters.duration);
+                 this._rotate((~~(angle*10))/10);
+             }
+             var self = this;
+             this._timer = setTimeout(function()
+                     {
+                     self._animate.call(self);
+                     }, 10);
+         }
+
+         // To fix Bug that prevents using recursive function in callback I moved this function to back
+         if (this._parameters.callback && checkEnd){
+             this._angle = this._parameters.animateTo;
+             this._rotate(this._angle);
+             this._parameters.callback.call(this._rootObj);
+         }
+     },
 
 	_rotate : (function()
 	{
@@ -342,17 +241,18 @@ Wilq32.PhotoEffect.prototype={
 		if (IE)
 		return function(angle)
 		{
-			this._container.style.rotation=angle+"deg";
+            this._angle = angle;
+			this._container.style.rotation=(angle%360)+"deg";
 		}
 		else if (supportedCSS)
 		return function(angle){
-			this._img.style[supportedCSS]="rotate("+angle+"deg)";
+            this._angle = angle;
+			this._img.style[supportedCSS]="rotate("+(angle%360)+"deg)";
 		}
 		else 
 		return function(angle)
 		{
-
-			if (!this._img.width||typeof angle!="number") return;
+            this._angle = angle;
 			angle=(angle%360)* rad;
 			// clear canvas	
 			this._canvas.width = this._width+this._widthAdd;
