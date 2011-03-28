e22a05eef2573f7606f27d6106df4ed0765ab5b4
wilq32 <wilq32@05f192a8-ba21-6a86-2ae6-0c881fb7de60>
2011-03-28 06:56:30 +0000

    Added MozTransform to use CSS3 in firefox, removed unused files, some minor ads changes

--- jQueryRotate3.js
+++ jQueryRotate3.js
@@ -9,7 +9,7 @@
 // Documentation removed from script file (was kinda useless and outdated)
 
 (function($) {
-var supportedCSS,styles=document.getElementsByTagName("head")[0].style,toCheck="transformProperty WebkitTransform OTransform msTransform MozTransform".split(" ");
+var supportedCSS,styles=document.getElementsByTagName("head")[0].style,toCheck="transformProperty WebkitTransform OTransform msTransform MozTransform".split(" "); //MozTransform <- firefox works slower with css!!!
 for (var a=0;a<toCheck.length;a++) if (styles[toCheck[a]] !== undefined) supportedCSS = toCheck[a];
 // Bad eval to preven google closure to remove it from code o_O
 // After compresion replace it back to var IE = 'v' == '\v'
@@ -48,38 +48,28 @@ Wilq32.PhotoEffect=(function(){
 
 	if (supportedCSS) {
 		return function(img,parameters){
-			img.Wilq32 = {
-				PhotoEffect: this
-			};
+			img.Wilq32 = {PhotoEffect: this};
             
             this._img = this._rootObj = this._eventObj = img;
             this._handleRotation(parameters);
 		}
-	} else {
+	} else if (IE) {
 		return function(img,parameters) {
 			// Make sure that class and id are also copied - just in case you would like to refeer to an newly created object
             this._img = img;
 
 			this._rootObj=document.createElement('span');
 			this._rootObj.style.display="inline-block";
-			this._rootObj.Wilq32 = 
-				{
-					PhotoEffect: this
-				};
+			this._rootObj.Wilq32 = {PhotoEffect: this};
 			img.parentNode.insertBefore(this._rootObj,img);
-			
-			if (img.complete) {
-				this._Loader(parameters);
-			} else {
-				var self=this;
-				// TODO: Remove jQuery dependency
-				jQuery(this._img).bind("load", function()
-				{
-					self._Loader(parameters);
-				});
-			}
+            this._Loader(parameters);
 		}
-	}
+	} else {
+        return function(img,parameters){
+            // Just for now... Dont do anything if CSS3 is not supported
+        this._rootObj = img;
+        }
+    }
 })();
 
 Wilq32.PhotoEffect.prototype={
@@ -122,79 +112,28 @@ Wilq32.PhotoEffect.prototype={
 		}
 	},
 
-	_Loader:(function()
-	{
-		if (IE)
-		return function(parameters)
+	_Loader: function(parameters)
 		{
 			var width=this._img.width;
 			var height=this._img.height;
-			this._img.parentNode.removeChild(this._img);
-							
-			this._vimage = this.createVMLNode('image');
-			this._vimage.src=this._img.src;
-			this._vimage.style.height=height+"px";
-			this._vimage.style.width=width+"px";
-			this._vimage.style.position="absolute"; // FIXES IE PROBLEM - its only rendered if its on absolute position!
-			this._vimage.style.top = "0px";
-			this._vimage.style.left = "0px";
+			//this._img.parentNode.removeChild(this._img);
+            //this._rootObj.parentNode.removeChild(this._rootObj);
+            
+            this._rootObj.appendChild(this._img);
 
-			/* Group minifying a small 1px precision problem when rotating object */
-			this._container =  this.createVMLNode('group');
-			this._container.style.width=width;
-			this._container.style.height=height;
-			this._container.style.position="absolute";
-			this._container.setAttribute('coordsize',width-1+','+(height-1)); // This -1, -1 trying to fix ugly problem with small displacement on IE
-			this._container.appendChild(this._vimage);
-			
-			this._rootObj.appendChild(this._container);
-			this._rootObj.style.position="relative"; // FIXES IE PROBLEM
-			this._rootObj.style.width=width+"px";
-			this._rootObj.style.height=height+"px";
-			this._rootObj.setAttribute('id',this._img.getAttribute('id'));
-			this._rootObj.className=this._img.className;			
+            this._rootObj.style.width = this._img.offsetWidth;
+            this._rootObj.style.height = this._img.offsetHeight;
+
+            this._img.style.position = "absolute";
+
+            this._rootObj = this._img;
+            this._rootObj.Wilq32 = {PhotoEffect: this}
+
+            this._rootObj.style.filter += "progid:DXImageTransform.Microsoft.Matrix(M11=1,M12=1,M21=1,M22=1,sizingMethod='auto expand')";
+
 		    this._eventObj = this._rootObj;	
-		    this._handleRotation(parameters);	
-		}
-		else
-		return function (parameters)
-		{
-			this._rootObj.setAttribute('id',this._img.getAttribute('id'));
-			this._rootObj.className=this._img.className;
-			
-			this._width=this._img.width;
-			this._height=this._img.height;
-			this._widthHalf=this._width/2; // used for optimisation
-			this._heightHalf=this._height/2;// used for optimisation
-			
-			var _widthMax=Math.sqrt((this._height)*(this._height) + (this._width) * (this._width));
-
-			this._widthAdd = _widthMax - this._width;
-			this._heightAdd = _widthMax - this._height;	// widthMax because maxWidth=maxHeight
-			this._widthAddHalf=this._widthAdd/2; // used for optimisation
-			this._heightAddHalf=this._heightAdd/2;// used for optimisation
-			
-			this._img.parentNode.removeChild(this._img);	
-			
-			this._aspectW = ((parseInt(this._img.style.width,10)) || this._width)/this._img.width;
-			this._aspectH = ((parseInt(this._img.style.height,10)) || this._height)/this._img.height;
-			
-			this._canvas=document.createElement('canvas');
-			this._canvas.setAttribute('width',this._width);
-			this._canvas.style.position="relative";
-			this._canvas.style.left = -this._widthAddHalf + "px";
-			this._canvas.style.top = -this._heightAddHalf + "px";
-			this._canvas.Wilq32 = this._rootObj.Wilq32;
-			
-			this._rootObj.appendChild(this._canvas);
-			this._rootObj.style.width=this._width+"px";
-			this._rootObj.style.height=this._height+"px";
-            this._eventObj = this._canvas;
-			
-			this._cnv=this._canvas.getContext('2d');
-            this._handleRotation(parameters);
-		}
-	})(),
+		    this._handleRotation(parameters);
+		},
 
 	_animateStart:function()
 	{	
@@ -243,49 +182,22 @@ _animate:function()
 		return function(angle)
 		{
             this._angle = angle;
-			this._container.style.rotation=(angle%360)+"deg";
+			//this._container.style.rotation=(angle%360)+"deg";
+            var _rad = angle * rad ;
+            costheta = Math.cos(_rad);
+            sintheta = Math.sin(_rad);
+            var fil = this._rootObj.filters.item("DXImageTransform.Microsoft.Matrix");
+            fil.M11=costheta; fil.M12=-sintheta; fil.M21=sintheta; fil.M22=costheta;
+
+            this._rootObj.style.marginLeft = -(this._rootObj.offsetWidth - this._rootObj.clientWidth)/2 +"px";
+            this._rootObj.style.marginTop = -(this._rootObj.offsetHeight - this._rootObj.clientHeight)/2 +"px";
 		}
 		else if (supportedCSS)
 		return function(angle){
             this._angle = angle;
 			this._img.style[supportedCSS]="rotate("+(angle%360)+"deg)";
 		}
-		else 
-		return function(angle)
-		{
-            this._angle = angle;
-			angle=(angle%360)* rad;
-			// clear canvas	
-			this._canvas.width = this._width+this._widthAdd;
-			this._canvas.height = this._height+this._heightAdd;
-						
-			// REMEMBER: all drawings are read from backwards.. so first function is translate, then rotate, then translate, translate..
-			this._cnv.translate(this._widthAddHalf,this._heightAddHalf);	// at least center image on screen
-			this._cnv.translate(this._widthHalf,this._heightHalf);			// we move image back to its orginal 
-			this._cnv.rotate(angle);										// rotate image
-			this._cnv.translate(-this._widthHalf,-this._heightHalf);		// move image to its center, so we can rotate around its center
-			this._cnv.scale(this._aspectW,this._aspectH); // SCALE - if needed ;)
-			this._cnv.drawImage(this._img, 0, 0);							// First - we draw image
-		}
 
 	})()
 }
-
-if (IE)
-{
-Wilq32.PhotoEffect.prototype.createVMLNode=(function(){
-document.createStyleSheet().addRule(".rvml", "behavior:url(#default#VML)");
-		try {
-			!document.namespaces.rvml && document.namespaces.add("rvml", "urn:schemas-microsoft-com:vml");
-			return function (tagName) {
-				return document.createElement('<rvml:' + tagName + ' class="rvml">');
-			};
-		} catch (e) {
-			return function (tagName) {
-				return document.createElement('<' + tagName + ' xmlns="urn:schemas-microsoft.com:vml" class="rvml">');
-			};
-		}		
-})();
-}
-
 })(jQuery);
